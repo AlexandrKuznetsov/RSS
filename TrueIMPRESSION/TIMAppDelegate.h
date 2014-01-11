4429f3a726060fc74ba2599dd03dd58f133a93ca
Vdovichenko Dmitry <dima.vdovichenko@gmail.com>
2014-01-11 21:12:31 +0200

    init commit

---
+++ TrueIMPRESSION/TIMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TIMAppDelegate.h
+//  TrueIMPRESSION
+//
+//  Created by Vdovichenko Dmitry on 11.01.14.
+//  Copyright (c) 2014 Impression. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TrueIMPRESSION/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TrueIMPRESSION
+//
+//  Created by Vdovichenko Dmitry on 11.01.14.
+//  Copyright (c) 2014 Impression. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TIMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TIMAppDelegate class]));
+    }
+}
