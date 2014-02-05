841ac6de1775491fbc748a2d932cb75812e6b753
Vdovichenko Dmitry <dima.vdovichenko@gmail.com>
2014-02-05 22:59:13 +0200

    some changes

--- Controllers/TIMLoginViewController.m
+++ Controllers/TIMLoginViewController.m
@@ -188,6 +188,7 @@
 - (void)hideKeyBoard {
     [self.loginField resignFirstResponder];
     [self.passwordField resignFirstResponder];
+    [self.mailField resignFirstResponder];
 }
 
 - (void)fieldsWithError {
